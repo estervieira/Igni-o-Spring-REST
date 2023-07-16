cb68c73211da65ad24ded34cc37c939e53cc4e71
estervieira <estervieirag450@gmail.com>
2023-07-16 18:57:20 -0300

    Evoluindo a API

--- src/main/java/com/algaworks/algatransito/domain/repository/ProprietarioRepository.java
+++ src/main/java/com/algaworks/algatransito/domain/repository/ProprietarioRepository.java
@@ -6,9 +6,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ProprietarioRepository extends JpaRepository<Proprietario, Long> {
     List<Proprietario> findByNomeContaining(String nome);
+    Optional<Proprietario> findByEmail(String email);
 
 }
--- src/main/java/com/algaworks/algatransito/domain/service/RegistroProprietarioService.java
+++ src/main/java/com/algaworks/algatransito/domain/service/RegistroProprietarioService.java
@@ -1,5 +1,6 @@
 package com.algaworks.algatransito.domain.service;
 
+import com.algaworks.algatransito.domain.exception.NegocioException;
 import com.algaworks.algatransito.domain.model.Proprietario;
 import com.algaworks.algatransito.domain.repository.ProprietarioRepository;
 import lombok.AllArgsConstructor;
@@ -12,8 +13,22 @@ public class RegistroProprietarioService {
 
     private final ProprietarioRepository proprietarioRepository;
 
+    public Proprietario buscar (Long proprietarioId) {
+        return proprietarioRepository.findById(proprietarioId)
+                .orElseThrow(() -> new NegocioException("Proprietário não encontrado"));
+    }
+
     @Transactional
     public Proprietario salvar(Proprietario proprietario){
+
+        boolean emailEmUso = proprietarioRepository.findByEmail(proprietario.getEmail())
+                .filter(p -> !p.equals(proprietario))
+                .isPresent();
+
+        if(emailEmUso){
+            throw new NegocioException("Já existe um proprietário cadastrado com esse email");
+        }
+
         return proprietarioRepository.save(proprietario);
     }
 
